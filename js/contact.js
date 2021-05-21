a1bb7040de4e3b8a393803be54bb105aacd0eaa7
Marius Hesby <marius.r.hesby@gmail.com>
2021-05-21 23:24:13 +0200

    Updates

--- js/contact.js
+++ js/contact.js
@@ -2,29 +2,23 @@
 const form = document.querySelector("#contactForm");
 const fullName = document.querySelector("#fullName");
 const fullNameError = document.querySelector("#fullNameError");
-const address = document.querySelector("#address");
-const addressError = document.querySelector("#addressError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
 const subject = document.querySelector("#subject");
 const subjectError = document.querySelector("#subjectError");
-const message = document.querySelector(".message");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+const messageSent = document.querySelector(".message");
+const updateButton = document.querySelector("main button");
 function validateForm(event) {
   // Prevent default form actions
   event.preventDefault();
 
   // Check full name
-  if (checkLength(fullName.value, 2)) {
+  if (checkLength(fullName.value, 6)) {
     fullNameError.style.display = "none";
   } else {
     fullNameError.style.display = "block";
-  }
-
-  // Check address
-  if (checkLength(address.value, 25)) {
-    addressError.style.display = "none";
-  } else {
-    addressError.style.display = "block";
   }
 
   // Check e-mail
@@ -39,6 +33,13 @@ function validateForm(event) {
     subjectError.style.display = "none";
   } else {
     subjectError.style.display = "block";
+  }
+
+  // Check message
+  if (checkLength(message.value, 26)) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
   }
 }
 
@@ -63,8 +64,11 @@ function validateEmail(email) {
 // Confirm that form has been sent
 function submitForm(event) {
   event.preventDefault();
-  if (checkLength(fullName.value, 0) && checkLength(address.value, 25) && validateEmail(email.value) && checkLength(subject.value, 10)) {
-    message.innerHTML = `<p class="sent">Your message has been sent</p>`;
+  if (checkLength(fullName.value, 6) && validateEmail(email.value) && checkLength(subject.value, 16) && checkLength(message.value, 26)) {
+    messageSent.innerHTML = `<p class="sent">Your message has been sent</p>`;
+    updateButton.innerHTML = "success";
+    updateButton.style.background = "green";
+
     // clear all input values
     form.reset();
   } else {
@@ -73,3 +77,7 @@ function submitForm(event) {
 }
 
 form.addEventListener("submit", submitForm);
+
+// updateButton.addEventListener("click", function () {
+//   updateButton.innerHTML = "Successfully updated!";
+// });
